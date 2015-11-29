33559edf284df7cb8d75f4ed48795f3f5e5eac41
Rasmus Larsson <rasmus.larsson@gmail.com>
2015-11-29 22:52:36 +0100

    DiscoBoxen Step 2.

--- exercises/disco-boxen/DiscoBoxen.elm
+++ exercises/disco-boxen/DiscoBoxen.elm
@@ -1,14 +1,46 @@
 module DiscoBoxen where
 
-import Color exposing (Color, black) --green, red, blue, yellow, brown, purple, orange)
-import Graphics.Element exposing (Element, spacer, color, flow, right)
+import Array exposing (fromList, get)
+import Color exposing (Color, black, white, green, red, blue, yellow, brown, purple, orange)
+import Graphics.Element exposing (Element, spacer, color, flow, right, show)
+import List exposing (foldr, (::))
+import Random exposing (generate, initialSeed)
+import Time exposing (every, millisecond, second)
 
 drawSquare : Int -> Int -> Color -> Element
 drawSquare x y c =
   spacer x y |> color c
 
 drawSquares : Int -> List Element
-drawSquares n =
-  List.map (\_ -> drawSquare 10 10 black) [1..n]
+drawSquares r =
+  foldr
+    (\x acc -> (::) 
+      (if r == x then
+        drawSquare 10 10 white
+      else
+        drawSquare 10 10 black)
+    acc)
+    []
+    [1..10]
 
-main = flow right (drawSquares 10)
+getColor n =
+  if n % 2 == 0 then
+    black
+  else
+    white
+
+getRandomBoxNum n =
+  let doGenerate seed =
+        generate (Random.int 1 10) (initialSeed (round seed)) |> fst
+      current = Signal.map doGenerate (every (n * millisecond))
+  in 
+    current
+
+{-getRandomBoxNum seed =
+  generate (Random.int 1 10) (initialSeed (round seed)) |> fst
+
+main = 
+  Signal.map (flow right) (Signal.map drawSquares (Signal.map getRandomBoxNum (every (200 * millisecond))))
+-}
+main = 
+  Signal.map (flow right) (Signal.map drawSquares (getRandomBoxNum 200))
