0893bde0a694688bdda79a7052cf93bf680c6c89
Rasmus Larsson <rasmus.larsson@gmail.com>
2015-11-29 14:06:42 +0100

    FizzBuzz Step 3.

--- exercises/fizzbuzz/FizzBuzz.elm
+++ exercises/fizzbuzz/FizzBuzz.elm
@@ -2,15 +2,20 @@ module FizzBuzz where
 
 import Graphics.Element exposing (show)
 
+fizzbuzz : Int -> String
 fizzbuzz n =
-  if n == 3 then
-    "Fizz"
-  else if n == 5 then
-    "Buzz"
-  else if n == 15 then
+  if mod n 15 then
     "FizzBuzz"
+  else if mod n 5 then
+    "Buzz"
+  else if mod n 3 then
+    "Fizz"
   else
     toString n
+
+mod : Int -> Int -> Bool
+mod i j =
+  i % j == 0
 
 main =
   show (List.map fizzbuzz [1..100])
